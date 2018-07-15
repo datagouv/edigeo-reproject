3a8c3328adf9fbf8cb65bb7e8f2b30c488216e3a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-07-15 22:45:21 +0200

    Fix issue with 0X departements

--- bin/cli.js
+++ bin/cli.js
@@ -12,7 +12,7 @@ if (!argv.d && !argv.dep) {
   process.exit(1)
 }
 
-const depCode = argv.d || argv.dep
+const depCode = (argv.d || argv.dep).toString().padStart(2, '0')
 
 getStream.buffer(process.stdin)
   .then(inputArchive => reprojectArchive(inputArchive, depCode, argv.m || argv.mode))
